0ed6b3c8dd3fada7cdb465ab7ec139da29e7cc98
jasmithadodda <jasmithadodda@gmail.com>
2024-08-14 15:14:10 +0530

    datafetching with reducer and managing the state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import DataFetchingOne from './hooks/useReducer/DataFetchingOne';
+import DataFetchingTwo from './hooks/useReducer/DataFetchingTwo';
 
 
 
 function App() {  return (
  <div>
-    <DataFetchingOne/>
+    <DataFetchingTwo/>
 </div>
   );
 }
