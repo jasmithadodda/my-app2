25de89b46219badefb8d13715a28739559d3bd90
jasmithadodda <jasmithadodda@gmail.com>
2024-08-14 15:11:47 +0530

    data fetching without reducer

--- src/App.js
+++ src/App.js
@@ -1,32 +1,11 @@
 import './App.css';
-import { createContext, useReducer} from "react";
-import ComponentA from './hooks/contextReducer/ComponentA';
-import ComponentB from './hooks/contextReducer/ComponentB';
-import ComponentC from './hooks/useContext/ComponentC'
+import DataFetchingOne from './hooks/useReducer/DataFetchingOne';
 
 
-export const CountContext = createContext();
-const initialState = 0
-const reducer = (state,action) => {
 
-  switch(action){
-      case 'increment': return state+1
-      case 'decrement' : return state -1
-      case 'reset':   return initialState
-      default: return state
-
-  }
-}
-
-function App() {
-  const [count,dispatch] = useReducer(reducer,initialState)
-  return (
+function App() {  return (
  <div>
-  <CountContext.Provider  value={{countState:count,countDispatch:dispatch}}>
-      <ComponentA/>
-      <ComponentB/>
-      <ComponentC/>
-  </CountContext.Provider>
+    <DataFetchingOne/>
 </div>
   );
 }
