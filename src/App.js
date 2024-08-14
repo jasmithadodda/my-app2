4a2beae12c389024dc941b08a207b6adf3ed17fc
jasmithadodda <jasmithadodda@gmail.com>
2024-08-14 12:50:40 +0530

    useContext with useReducer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import './App.css';
 import { createContext } from "react";
-import CounterThree from './hooks/useReducer/CounterThree';
+import ComponentA from './hooks/contextReducer/ComponentA';
+import ComponentB from './hooks/contextReducer/ComponentB';
+import ComponentC from './hooks/useContext/ComponentC'
 
 
 export const ThemeContext = createContext(null);
+const initialState = 0
+const reducer = (state,action) => {
+
+  switch(action){
+      case 'increment': return state+1
+      case 'decrement' : return state -1
+      case 'reset':   return initialState
+      default: return state
+
+  }
+}
 
 function App() {
   return (
  <div>
-      <CounterThree/>
+      <ComponentA/>
+      <ComponentB/>
+      <ComponentC/>
 </div>
   );
 }
